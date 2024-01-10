83683035036a1283e24ea08159624d3a8ef38829
meerim <mmusaeva2023@gmail.com>
2024-01-09 21:02:43 -0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  const boysName = ["Dastan", "Azim", "Tima"];
+  const girlNames = ["Meka", "Aida", "Aigerim", "Erkinai", "Janara"]
+  
+  const showBoysNames = () => {
+    return boysName.map((el,index) => <p className='boy' key={index}> {(el)}</p>)
+  };
+
+  const showGirl = () => {
+    return girlNames.map((el, index) => <p className='girl' key={index}> { (el)}</p> )
+  }
+
   return (
     <div className="App">
-      <h1>heloo</h1>
+      <div className='boys'>
+        {
+        showBoysNames()}
+      </div>
+      <div className='girls'>
+        {showGirl()}
+      </div>
     </div>
   );
 }
